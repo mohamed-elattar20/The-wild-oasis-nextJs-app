eab9acc6285bcce07e76e148d09b7e9ca274d82d
mohamed-elattar20 <mohamedelattarm819@gmail.com>
2025-12-17 19:25:15 +0200

    feat: added signin flow to app

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,16 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
+  const firstname = session?.user?.name?.split(" ")[0];
   return (
     <>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome Jonas
+        Welcome, {firstname}
       </h2>
     </>
   );
