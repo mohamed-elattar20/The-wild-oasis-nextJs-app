9bf98444812647045486ec4b5a63025f1abaf31b
mohamed-elattar20 <mohamedelattarm819@gmail.com>
2025-12-18 13:45:41 +0200

    feat: added signin callback to connect the current user to an existing guest otherwise will create a new guest with the information

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -11,6 +12,25 @@ const authConfig = {
   callbacks: {
     authorized({ auth, reuqest }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(account.email);
+        if (!existingGuest)
+          await createGuest({
+            fullName: user.name,
+            email: user.email,
+          });
+        return true;
+      } catch {
+        return false; // prevent sign in if guest not found
+      }
+    },
+    async session({ session, token, user }) {
+      const guest = getGuest(session.user.email);
+      // Attach additional properties to the session object here
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
